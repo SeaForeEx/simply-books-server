dd38333ea0d2d5d8b551b485609beaabc9a77969
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-05-07 12:47:49 -0500

    No such table, wrote a ticket

--- simply.sql
+++ simply.sql
@@ -8,9 +8,9 @@ CREATE TABLE `Author_Book` (
 
 CREATE TABLE `Author` (
     `id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `email` TEXT NOT NULL,
     `first_name` TEXT NOT NULL,
     `last_name` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
     `image` TEXT NOT NULL,
     `favorite`  BOOLEAN NOT NULL  
 );
@@ -35,27 +35,28 @@ INSERT INTO `Author_Book` VALUES (null, 8, 8)
 INSERT INTO `Author_Book` VALUES (null, 9, 9)
 INSERT INTO `Author_Book` VALUES (null, 10, 10)
 
-insert into `Author` values (null, 'Edwina', 'Curee', 'ecuree0@skype.com', 'http://dummyimage.com/217x100.png/cc0000/ffffff', false);
-insert into `Author` values (null, 'Wes', 'Yeates', 'wyeates1@webnode.com', 'http://dummyimage.com/250x100.png/5fa2dd/ffffff', false);
-insert into `Author` values (null, 'Kipper', 'Stockford', 'kstockford2@hubpages.com', 'http://dummyimage.com/128x100.png/ff4444/ffffff', true);
-insert into `Author` values (null, 'Arleen', 'Avramow', 'aavramow3@google.cn', 'http://dummyimage.com/200x100.png/cc0000/ffffff', false);
-insert into `Author` values (null, 'Rubetta', 'McQuorkel', 'rmcquorkel4@furl.net', 'http://dummyimage.com/186x100.png/cc0000/ffffff', false);
-insert into `Author` values (null, 'Cristobal', 'Anstie', 'canstie5@flickr.com', 'http://dummyimage.com/103x100.png/ff4444/ffffff', true);
-insert into `Author` values (null, 'Nikki', 'Duckett', 'nduckett6@wsj.com', 'http://dummyimage.com/154x100.png/cc0000/ffffff', false);
-insert into `Author` values (null, 'Fredi', 'Daugherty', 'fdaugherty7@goo.gl', 'http://dummyimage.com/248x100.png/cc0000/ffffff', true);
-insert into `Author` values (null, 'Englebert', 'Beneix', 'ebeneix8@cbc.ca', 'http://dummyimage.com/181x100.png/cc0000/ffffff', false);
-insert into `Author` values (null, 'Catrina', 'Kleimt', 'ckleimt9@guardian.co.uk', 'http://dummyimage.com/161x100.png/5fa2dd/ffffff', true);
+INSERT INTO `Author` VALUES (null, 'Edwina', 'Curee', 'ecuree0@skype.com', 'http://dummyimage.com/217x100.png/cc0000/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Wes', 'Yeates', 'wyeates1@webnode.com', 'http://dummyimage.com/250x100.png/5fa2dd/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Kipper', 'Stockford', 'kstockford2@hubpages.com', 'http://dummyimage.com/128x100.png/ff4444/ffffff', 1);
+INSERT INTO `Author` VALUES (null, 'Arleen', 'Avramow', 'aavramow3@google.cn', 'http://dummyimage.com/200x100.png/cc0000/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Rubetta', 'McQuorkel', 'rmcquorkel4@furl.net', 'http://dummyimage.com/186x100.png/cc0000/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Cristobal', 'Anstie', 'canstie5@flickr.com', 'http://dummyimage.com/103x100.png/ff4444/ffffff', 1);
+INSERT INTO `Author` VALUES (null, 'Nikki', 'Duckett', 'nduckett6@wsj.com', 'http://dummyimage.com/154x100.png/cc0000/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Fredi', 'Daugherty', 'fdaugherty7@goo.gl', 'http://dummyimage.com/248x100.png/cc0000/ffffff', 1);
+INSERT INTO `Author` VALUES (null, 'Englebert', 'Beneix', 'ebeneix8@cbc.ca', 'http://dummyimage.com/181x100.png/cc0000/ffffff', 0);
+INSERT INTO `Author` VALUES (null, 'Catrina', 'Kleimt', 'ckleimt9@guardian.co.uk', 'http://dummyimage.com/161x100.png/5fa2dd/ffffff', 1);
 
-insert into `Book` values (null, 'George Lopez: It''s Not Me, It''s You', 'http://dummyimage.com/133x100.png/ff4444/ffffff', '$6.01', false, 'Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.');
-insert into `Book` values (null, 'Takva: A Man''s Fear of God', 'http://dummyimage.com/137x100.png/dddddd/000000', '$6.97', true, 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus.');
-insert into `Book` values (null, 'Rebecca of Sunnybrook Farm', 'http://dummyimage.com/159x100.png/ff4444/ffffff', '$3.23', false, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.');
+
+insert into `Book` values (null, 'George Lopez: It''s Not Me, It''s You', 'http://dummyimage.com/133x100.png/ff4444/ffffff', 6.01, 0, 'Suspendisse potenti. Nullam porttitor lacus at turpis.');
+insert into `Book` values (null, 'Takva: A Man''s Fear of God', 'http://dummyimage.com/137x100.png/dddddd/000000', 6.97, 1, 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.');
+insert into `Book` values (null, 'Rebecca of Sunnybrook Farm', 'http://dummyimage.com/159x100.png/ff4444/ffffff', 3.23, 0, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. ');
 insert into `Book` values (null, 'Police Academy: Mission to Moscow', 'http://dummyimage.com/103x100.png/5fa2dd/ffffff', '$3.41', false, 'Aenean lectus.');
-insert into `Book` values (null, 'Unspeakable Acts ', 'http://dummyimage.com/219x100.png/5fa2dd/ffffff', '$2.12', true, 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus.');
-insert into `Book` values (null, 'Wu yen', 'http://dummyimage.com/242x100.png/5fa2dd/ffffff', '$4.19', true, 'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim.');
-insert into `Book` (null, 'Without a Clue', 'http://dummyimage.com/194x100.png/cc0000/ffffff', '$6.41', false, 'Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
-insert into `Book` values (null, 'Europa Europa (Hitlerjunge Salomon)', 'http://dummyimage.com/206x100.png/dddddd/000000', '$7.66', false, 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.');
-insert into `Book` values (null, 'To Be or Not to Be', 'http://dummyimage.com/244x100.png/5fa2dd/ffffff', '$3.26', true, 'Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
-insert into `Book` values (null, 'Solino', 'http://dummyimage.com/246x100.png/cc0000/ffffff', '$3.13', true, 'Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui.');
+insert into `Book` values (null, 'Unspeakable Acts ', 'http://dummyimage.com/219x100.png/5fa2dd/ffffff', 2.12, 1, 'Aliquam quis turpis eget elit sodales scelerisque. ');
+insert into `Book` values (null, 'Wu yen', 'http://dummyimage.com/242x100.png/5fa2dd/ffffff', 4.19, 1, 'Vivamus vestibulum sagittis sapien.');
+insert into `Book` (null, 'Without a Clue', 'http://dummyimage.com/194x100.png/cc0000/ffffff', 6.41, 0, 'Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
+insert into `Book` values (null, 'Europa Europa (Hitlerjunge Salomon)', 'http://dummyimage.com/206x100.png/dddddd/000000', 7.66, 0, 'Integer ac leo. Pellentesque ultrices mattis odio. ');
+insert into `Book` values (null, 'To Be or Not to Be', 'http://dummyimage.com/244x100.png/5fa2dd/ffffff', 3.26, 1, 'Suspendisse potenti. Cras in purus eu magna vulputate luctus. ');
+insert into `Book` values (null, 'Solino', 'http://dummyimage.com/246x100.png/cc0000/ffffff', 3.13, 1, 'Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui.');
 
 SELECT
     a.id,
@@ -66,3 +67,7 @@ SELECT
     a.favorite
 FROM author a
 WHERE a.id = 3
+
+SELECT * FROM Author a ORDER BY id DESC
+
+SELECT name FROM sqlite_master WHERE type='table' AND name='Author';
